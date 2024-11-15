458c705870e0e35f3760c22fc8479035ce2a0fa5
dian-web31 <juan.henao36071@tools.ucaldas.edu.co>
2024-11-15 05:08:55 -0500

    add url

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const API_URL = "http://127.0.0.1:5000/login";
+const API_URL = "cht-voi-bot-ffc9a4fmgfeedmce.eastus-01.azurewebsites.net/login";
 
 document.getElementById("loginForm").addEventListener("submit", async (event) => {
     event.preventDefault();
